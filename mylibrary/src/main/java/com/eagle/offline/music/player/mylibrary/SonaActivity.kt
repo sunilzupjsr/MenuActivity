3e928ba1eccbb0341a39d3711bf8c68781283711
Sunil Kumar <sunil.kumar@bharatzup.com>
2022-11-28 20:53:31 +0530

    Initial commit

---
+++ mylibrary/src/main/java/com/eagle/offline/music/player/mylibrary/SonaActivity.kt
@@ -0,0 +1,11 @@
+package com.eagle.offline.music.player.mylibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SonaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sona)
+    }
+}
